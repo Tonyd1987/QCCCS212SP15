0200a72f41f2c50e093beb120c9a40be84627189
ian <ian@ian-THINK>
2013-01-28 07:34:19 -0500

    Updated the For Loop to make it more user friendly.

--- src/edu/mass/qcc/qcccodewizard/VariableTypeComboBoxModel.java
+++ src/edu/mass/qcc/qcccodewizard/VariableTypeComboBoxModel.java
@@ -16,7 +16,7 @@ import javax.swing.ComboBoxModel;
  */
 class VariableTypeComboBoxModel extends AbstractListModel implements ComboBoxModel {
     String[] VariableTypes = {
-        "Int", "String", "Char", "Short", "Long", "Float", "Double", "Boolean"
+        "int", "String", "char", "short", "long", "float", "double", "boolean"
     };
     String   selection     = null;
 
